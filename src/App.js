1b8e0a0881f0cf678489b675482f46b9ae400a9c
HasibGit <hasibullah04@gmail.com>
2024-08-03 22:04:36 +0600

    working on the sidebar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Lets bridge!</h1>
-    </div>
-  );
+  return <div className="app"></div>;
 }
 
 export default App;
