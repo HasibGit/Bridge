200ccd4c964745d5f9e5b08f4b10676200491212
HasibGit <hasibullah04@gmail.com>
2024-08-03 19:55:44 +0600

    brought in firebase config. removed unnecessary files

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBdhDhx60y4aHAK803llCs9BXPcoGvRSzE",
+  authDomain: "bridge-e065d.firebaseapp.com",
+  projectId: "bridge-e065d",
+  storageBucket: "bridge-e065d.appspot.com",
+  messagingSenderId: "652618253505",
+  appId: "1:652618253505:web:b297f9bbb598bd23fc8040",
+};
