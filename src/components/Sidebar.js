c7d65eab440250cca85589d1f81ea62e18b3c613
HasibGit <hasibullah04@gmail.com>
2024-08-03 23:40:59 +0600

    sidebar header section design done

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "./Sidebar.css";
+import { Avatar, Button } from "antd";
+import {
+  UserOutlined,
+  MessageOutlined,
+  MenuOutlined,
+  ChromeOutlined,
+} from "@ant-design/icons";
 
 function Sidebar() {
   return (
-    <div>
-      <div className="sidebar_header"></div>
+    <div className="sidebar">
+      <div className="sidebar_header">
+        <Avatar size={50} icon={<UserOutlined />} />
+        <div className="sidebar_headerRight">
+          <Button shape="circle" icon={<ChromeOutlined />} />
+          <Button shape="circle" icon={<MessageOutlined />} />
+          <Button shape="circle" icon={<MenuOutlined />} />
+        </div>
+      </div>
       <div className="sidebar_search"></div>
       <div className="sidebar_chats"></div>
     </div>
