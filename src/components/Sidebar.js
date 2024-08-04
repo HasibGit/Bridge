c2f0f5f477829eb29d64a4484873a46a37cfb33a
HasibGit <hasibullah04@gmail.com>
2024-08-04 14:04:50 +0600

    revamped room create modal. Working on chat component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sidebar.css";
-import { Avatar, Button, Input, Tooltip } from "antd";
+import { Avatar, Button, Input, Tooltip, Modal } from "antd";
 import {
   UserOutlined,
   MessageOutlined,
@@ -11,42 +11,67 @@ import {
 import SidebarChat from "./SidebarChat";
 
 function Sidebar() {
-  const createChat = () => {
-    const roomName = prompt("Enter room name");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [roomName, setRoomName] = useState("");
 
+  const createChat = () => {
     if (roomName) {
       console.log("Room being created");
     }
+    setIsModalOpen(false);
+  };
+
+  const handleModalState = () => {
+    setIsModalOpen(true);
   };
 
   return (
-    <div className="sidebar">
-      <div className="sidebar_header">
-        <Avatar size={50} icon={<UserOutlined />} />
-        <div className="sidebar_headerRight">
-          <Tooltip title="Create room">
-            <Button
-              shape="circle"
-              icon={<PlusSquareOutlined />}
-              onClick={createChat}
-            />
-          </Tooltip>
+    <>
+      <Modal
+        title="Create Room"
+        open={isModalOpen}
+        onOk={createChat}
+        onCancel={() => {
+          setIsModalOpen(false);
+        }}
+        closable="true"
+        okButtonProps={{ disabled: !roomName }}
+      >
+        <Input
+          placeholder="Enter room name"
+          value={roomName}
+          onChange={(event) => setRoomName(event.target.value)}
+          style={{ margin: "16px 0" }}
+        />
+      </Modal>
+      <div className="sidebar">
+        <div className="sidebar_header">
+          <Avatar size={50} icon={<UserOutlined />} />
+          <div className="sidebar_headerRight">
+            <Tooltip title="Create room">
+              <Button
+                shape="circle"
+                icon={<PlusSquareOutlined />}
+                onClick={handleModalState}
+              />
+            </Tooltip>
 
-          <Button shape="circle" icon={<MessageOutlined />} />
-          <Button shape="circle" icon={<MenuOutlined />} />
+            <Button shape="circle" icon={<MessageOutlined />} />
+            <Button shape="circle" icon={<MenuOutlined />} />
+          </div>
+        </div>
+        <div className="sidebar_search">
+          <Input placeholder="Search" prefix={<SearchOutlined />} />
+        </div>
+        <div className="sidebar_chats">
+          <SidebarChat />
+          <SidebarChat />
+          <SidebarChat />
+          <SidebarChat />
+          <SidebarChat />
         </div>
       </div>
-      <div className="sidebar_search">
-        <Input placeholder="Search" prefix={<SearchOutlined />} />
-      </div>
-      <div className="sidebar_chats">
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
-      </div>
-    </div>
+    </>
   );
 }
 
