45ada8afc4929506906732cb4d227c39c6307993
HasibGit <hasibullah04@gmail.com>
2024-08-07 01:22:08 +0600

    room last message feature implemented

--- src/components/SidebarChat.js
+++ src/components/SidebarChat.js
@@ -2,13 +2,27 @@ import React, { useEffect, useState } from "react";
 import "./SidebarChat.css";
 import { Avatar } from "antd";
 import { Link } from "react-router-dom";
+import db from "../config/firebaseConfig";
 
 function SidebarChat({ id, name }) {
   const [seed, setSeed] = useState("");
+  const [lastMessage, setLastMessage] = useState("");
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
+
+  useEffect(() => {
+    if (id) {
+      db.collection("rooms")
+        .doc(id)
+        .collection("messages")
+        .orderBy("timestamp", "desc")
+        .onSnapshot((snapshot) =>
+          setLastMessage(snapshot?.docs[0]?.data()?.message || "")
+        );
+    }
+  });
 
   return (
     <Link to={`/rooms/${id}`}>
@@ -18,7 +32,7 @@ function SidebarChat({ id, name }) {
         />
         <div className="sidebarChat_info">
           <h2>{name}</h2>
-          <p>Last message</p>
+          <p>{lastMessage}</p>
         </div>
       </div>
     </Link>
